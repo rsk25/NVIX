7c162e54dd542c2a39a4b55e3ebea890edd955e2
rsk25 <montgomery7289@gmail.com>
2022-01-27 12:49:26 +0000

    reordered explanation mapping so that the explanations fit with alignments of the attention scores (quick-fix, needs more robust solution...)

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -3,7 +3,7 @@ import torch
 from typing import List, Dict
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-
+from collections import OrderedDict
 
 def set_ckpt_path(dir: str, from_pretrained: bool=True):
     if from_pretrained:
@@ -15,6 +15,18 @@ def set_ckpt_path(dir: str, from_pretrained: bool=True):
 def _save_figure(figure_name: str, format: str='png'):
     file_name = Path('.') / 'figures' / f'{figure_name}.{format}'
     plt.savefig(file_name, bbox_inches='tight', facecolor='white')
+
+
+def _reorder_mapping(orig_map: Dict[str, List[str]]) -> Dict[str, List[str]]:
+    new_map = OrderedDict()
+    for k, v in orig_map.items():
+        if k[0] == 'X':
+            new_map[k] = v[0].split()
+    for k, v in orig_map.items():
+        if k[0] == 'N':
+            new_map[k] = v[0].split()
+    print(new_map)
+    return new_map
 
 
 def display_heatmap(sentence: List[str], 
@@ -32,24 +44,24 @@ def display_heatmap(sentence: List[str],
 
     fig = plt.figure(figsize=(40, 40))
 
+    new_map = _reorder_mapping(explanation)
     begin_idx = 0
     end_idx = 0
-    for i, k in enumerate(explanation):
-        expl_list = explanation[k][0].split()+['<EOS>']
+    for i, k in enumerate(new_map):
+        expl_list = new_map[k]+['<EOS>'] # explanation mapping과 python이 iter하는 방법이 일치하지 않을 수도
         end_idx = begin_idx + len(expl_list)
         
         # attention heatmap
         ax1 = fig.add_subplot(n_rows, n_cols*2, i*2+1) 
 
-        _attn_for_expl = _attention[begin_idx:end_idx, :]
-        _attn_for_expl = _attn_for_expl.numpy()
+        _attn_for_expl = _attention[begin_idx:end_idx, :].numpy()
 
         cax1 = ax1.matshow(_attn_for_expl, cmap='bone')
         fig.colorbar(cax1, location='bottom')
 
         ax1.tick_params(labelsize=12)
-        ax1.set_xticklabels(['']+['<SOS>']+[t for t in sentence]+['<EOS>'], 
-                           rotation=45)
+        ax1.set_xticklabels(['']+['<BOS>']+[t for t in sentence]+['<EOS>'], 
+                           rotation=90)
         ax1.set_yticklabels(['']+expl_list)
         ax1.set_title(k)
         ax1.set_xlabel('Source Sentence')
@@ -62,8 +74,7 @@ def display_heatmap(sentence: List[str],
         # copy probability heatmap
         ax2 = fig.add_subplot(n_rows, n_cols*2, i*2+2)
 
-        _probs_for_expl = _copy_probs[begin_idx:end_idx]
-        _probs_for_expl = _probs_for_expl.unsqueeze(0).numpy()
+        _probs_for_expl = _copy_probs[begin_idx:end_idx].unsqueeze(0).numpy()
 
         cax2 = ax2.matshow(_probs_for_expl, cmap='bone')
         fig.colorbar(cax2, location='bottom')
